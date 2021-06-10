3ba149ddddb24511b9e6ae7d1549ed617a9ecf94
zayedibnibrahim <zayed.ibn@gmail.com>
2021-06-11 02:05:06 +0600

    Added Other pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
+import Footer from './components/Footer';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
   return (
-    <div className="container mx-auto px-100">
-      <h1 className="text-blue-900">Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar toggle={toggle} />
+        <Dropdown isOpen={isOpen} toggle={toggle} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/menu' component={Menu} />
+          <Route path='/about' component={About} />
+        </Switch>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
